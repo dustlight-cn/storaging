497d2754679c1b2d629b8d6e275b0415a23a2d68
Hansin1997 <845612500@qq.com>
2021-07-08 10:20:34 +0800

    Swagger UI

--- storage-service/src/main/java/plus/storage/application/configurations/SecurityConfig.java
+++ storage-service/src/main/java/plus/storage/application/configurations/SecurityConfig.java
@@ -1,10 +1,21 @@
 package plus.storage.application.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import plus.auth.resources.AuthSecurityWebFilterChainConfiguration;
 
 @EnableReactiveMethodSecurity
 @Configuration
 public class SecurityConfig extends AuthSecurityWebFilterChainConfiguration {
+
+    @Override
+    protected ServerHttpSecurity configure(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .pathMatchers("/index").authenticated()
+                .anyExchange().permitAll()
+                .and();
+    }
 }
--- storage-service/src/main/java/plus/storage/application/controllers/TestController.java
+++ storage-service/src/main/java/plus/storage/application/controllers/TestController.java
@@ -1,5 +1,6 @@
 package plus.storage.application.controllers;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,11 @@ import reactor.core.publisher.Mono;
 import java.security.Principal;
 
 @RestController
+@SecurityRequirement(name = "auth")
 public class TestController {
 
     @PreAuthorize("hasRole('Root')")
-    @GetMapping("/")
+    @GetMapping("/index")
     public Mono<Object> index(Principal principal){
         return Mono.just(principal);
     }
