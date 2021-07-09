8469249cba10b3b26f42808eac302841b7c8bf08
Hansin1997 <845612500@qq.com>
2021-07-09 13:35:43 +0800

    [UPDATE] 更新 OpenAPI。

--- storage-service/src/main/java/plus/storage/application/configurations/StorageServiceConfiguration.java
+++ storage-service/src/main/java/plus/storage/application/configurations/StorageServiceConfiguration.java
@@ -1,7 +1,6 @@
 package plus.storage.application.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import plus.storage.cloud.services.CloudStorageService;
